b721eb6902a166a15606dc0ddbcb480ff20ee59c
katerhoades <katerhoades@gmail.com>
2019-11-12 14:05:06 -0800

    d

---
+++ Sparkler/other.js
@@ -0,0 +1,22 @@
+var bg, spark;
+
+function preload()
+{
+  // load images
+  bg = loadImage("https://github.com/katerhoades/Sparkler/blob/master/RiverNight.jpg");
+}
+
+function setup()
+{
+  // set canvas size
+  createCanvas(windowWidth,windowHeight);
+
+  // hide mouse cursor
+
+function draw()
+
+  // display background image
+  image(bg, 10, 10);
+
+  // display sparkler
+}
