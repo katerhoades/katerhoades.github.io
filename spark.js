e54b4fa474052e8d75b361c7484924914cb5e296
katerhoades <katerhoades@gmail.com>
2019-11-12 21:29:09 -0800

    Update spark.js

--- spark.js
+++ spark.js
@@ -7,6 +7,7 @@ function setup() {
 }
 function draw() {
     clear();
+    noCursor();
     image(kitty, mouseX, mouseY,
         300+sin(frameCount*0.05)*100,
         300+cos(frameCount*0.05)*100);
