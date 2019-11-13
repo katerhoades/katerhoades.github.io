10d54d29f30c64218b2b402b158582e98292d291
katerhoades <katerhoades@gmail.com>
2019-11-12 20:07:32 -0800

    h

--- spark.js
+++ spark.js
@@ -3,11 +3,11 @@ function preload() {
   kitty = loadImage("spider.png");
 }
 function setup() {
-  createCanvas(windowWidth-20, windowHeight-20);
+  createCanvas(windowWidth-10, windowHeight-10);
 }
 function draw() {
 
-  imageMode(CENTER);
+
   image(kitty, 200+sin(frameCount*0.01)*100,
     200+cos(frameCount*0.05)*100);
   image(kitty, 100+cos(frameCount*0.01)*100,
