e4b6442289d2c0225bd2ac26c7c4c9375b337005
katerhoades <katerhoades@gmail.com>
2019-11-12 19:00:43 -0800

    Update spark.js

--- spark.js
+++ spark.js
@@ -1,18 +1,9 @@
-
-var img8;
-
-function preload() {
-
-img8 = loadImage("SparkVid.gif");
-
-}
 function setup() {
-  createCanvas(300, 300);
+  createCanvas(400, 400);
 }
-function draw() {
 
-    image(img8, 30, 30);
-    rect (20,10,10,20)
-  }
+function draw() {
+  background(220);
+  rect (30,30,20,10);
 
 }
