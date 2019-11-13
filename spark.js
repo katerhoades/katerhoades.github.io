ca2e293788425043c27cd306e8841b299b190d37
katerhoades <katerhoades@gmail.com>
2019-11-12 21:22:15 -0800

    Update spark.js

--- spark.js
+++ spark.js
@@ -3,12 +3,12 @@ function preload() {
   kitty = loadImage("spider.png");
 }
 function setup() {
-  createCanvas(600,600);
+  createCanvas(400, 400);
 }
 function draw() {
-
-noCursor();
-noStroke();
-clear();
-  image(kitty, mouseX, mouseY);
+    background(255);
+    imageMode(CENTER);
+    image(kitty, mouseX, mouseY,
+        200+sin(frameCount*0.1)*100,
+        200+cos(frameCount*0.1)*100);
 }
