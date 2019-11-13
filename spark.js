3c56857c2a8e47307a6fe32ce1ed0347288896df
katerhoades <katerhoades@gmail.com>
2019-11-12 21:25:38 -0800

    s

--- spark.js
+++ spark.js
@@ -6,8 +6,7 @@ function setup() {
   createCanvas(windowWidth-20, windowHeight-20);
 }
 function draw() {
-    background(255);
-    imageMode(CENTER);
+    clear();
     image(kitty, mouseX, mouseY,
         200+sin(frameCount*0.1)*100,
         200+cos(frameCount*0.1)*100);
