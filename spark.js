20e35315b2633d1e66cfb08aaa3b6ac0fa22e1e0
katerhoades <katerhoades@gmail.com>
2019-11-12 19:59:19 -0800

    Update spark.js

--- spark.js
+++ spark.js
@@ -10,6 +10,6 @@ function draw() {
   imageMode(CENTER);
   image(kitty, 200+sin(frameCount*0.01)*100,
     200+cos(frameCount*0.05)*100);
-  image(kitty, 200+cos(frameCount*0.01)*100,
+  image(kitty, 100+cos(frameCount*0.01)*100,
     200+sin(frameCount*0.06)*100);
 }
