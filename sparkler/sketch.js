c4dc8d6ae155364d27f1afeb2105aed3d4d02d90
katerhoades <katerhoades@gmail.com>
2019-11-12 17:24:17 -0800

    Create sketch.js

---
+++ sparkler/sketch.js
@@ -0,0 +1,28 @@
+var bg, spark;
+
+function preload()
+{
+// load images
+bg = loadImage("sparkler\RiverNight.jpg");
+spark = loadImage("sparkler\SparkVid.gif");
+}
+
+function setup()
+{
+// set canvas size
+createCanvas(400,400);
+
+// hide mouse cursor
+noCursor();
+}
+
+function draw()
+{
+
+// display background image
+image(bg, 0, 0);
+
+// display ufo
+image(spark, mouseX, mouseY);
+
+}
